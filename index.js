b14ca0d9a51abcbe48ddc747ca22640caeeb56dd
Piotr Winiarski <pwiniarski1991@gmail.com>
2018-09-11 22:47:48 +0200

    adding webpack to project, basic structure of project

--- index.js
+++
@@ -1,6 +0,0 @@
-import react from 'react';
-import reactDOM from 'react-dom';
-import App from 'app'
-
-
-ReactDOM.render(App,document.getElementById('rootContainer'));
--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import react from 'react';
+import ReactDOM from 'react-dom';
+import "./App.css";
 
 export default class App extends react.Component {
 
